29ea93cd6ac6d29f1008c90d81475551b06b8343
anfi2739 <anfi2739@colorado.edu>
2025-10-26 21:24:27 -0600

    w10

---
+++ wa/wa10.html/script.js
@@ -0,0 +1,36 @@
+let triviabtn = document.querySelector("#js-new-quote").addEventListener("click", newTrivia);
+
+let answerbtn = document.querySelector("#js-tweet").addEventListener("click", newAnswer);
+
+let current = {
+    question: "",
+    answer: "",
+}
+
+const endpoint = "https://catfact.ninja/fact";
+
+async function newTrivia() {
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        const data = await response.json();
+        current.question = data.fact;
+        displayTrivia();
+    } catch (err) {
+        alert('Failed to get trivia');
+        console.error(err);
+    }
+}
+
+function displayTrivia() {
+    const triviaText = document.querySelector("#js-quote-text");
+    triviaText.textContent = current.question;
+}
+
+function newAnswer() {
+    alert("Cats are amazing, aren’t they?");
+}
+newTrivia();
